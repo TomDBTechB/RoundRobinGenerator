84d3ea86510cfd1973c58b21cccb42d1ae08d659
Unknown <tom.debievre@gmail.com>
2019-02-22 22:15:28 +0100

    Choco CSV Writer + jar builder for countsport port

--- choco/src/main/java/CsvWriter.java
+++ choco/src/main/java/CsvWriter.java
@@ -1,12 +1,60 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.chocosolver.solver.variables.IntVar;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class CsvWriter {
     private static final String NEW_LINE_SEPARATOR = "\n";
 
 
-    public static void createSample(IntVar[][][] matchdays,int amountOfMatchDays,int amountOfTeams){
+    public static void createSample(IntVar[][][] matchdays,int amountOfMatchDays,int amountOfTeams,int counter){
+        String fileName ="./"+ amountOfTeams+"T_"+counter+".csv";
+        try(
+                BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileName));
+                CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT)
+        ) {
+
+         ArrayList<String> headerList = new ArrayList<String>();
+         ArrayList<String> headerList2 = new ArrayList<String>();
+
+         headerList.add("");
+         headerList2.add("");
+
+         for (int j = 0; j<amountOfMatchDays;j++){
+             for(int i=0;i<amountOfTeams;i++){
+                 headerList.add("M"+j);
+                 int index = i+1;
+                 headerList2.add("T"+index);
+
+             }
+         }
+
+         printer.printRecord(headerList);
+         printer.printRecord(headerList2);
+
+         for(int teamindex = 0;teamindex<amountOfTeams;teamindex++){
+            ArrayList<String> teamlist = new ArrayList<String>();
+            int index = teamindex+1;
+            teamlist.add("T"+index);
+            for(int matchday=0;matchday<amountOfMatchDays;matchday++){
+            for(int hometeam =0;hometeam<amountOfTeams;hometeam++){
+                teamlist.add(matchday+String.valueOf(matchdays[matchday][hometeam][teamindex].getValue()));
+                }
+            }
+            printer.printRecord(teamlist);
+            printer.flush();
 
 
+         }
+
+        } catch (IOException e) {
+            System.err.println("Fatal error when opening/writing to "+fileName);
+        }
 
     }
 
--- choco/src/main/java/DoubleRoundRobinGenerator.java
+++ choco/src/main/java/DoubleRoundRobinGenerator.java
@@ -5,41 +5,42 @@ import org.chocosolver.solver.variables.IntVar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Timer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DoubleRoundRobinGenerator {
 
-    private static int AMOUNT_OF_TEAMS = 6;
 
     public static void main(String[] args) {
+        int amountOfTeams = 6; //Integer.parseInt(args[0]);
+        int amountOfSamples = 1; //Integer.parseInt(args[1]);
+        //String sampleDirectory = args[2];
         StopWatch watch = new StopWatch();
         watch.start();
-        int amountOfMatchDays = calculateMatchDays();
+        int amountOfMatchDays = calculateMatchDays(amountOfTeams);
 
 
         //modelgenerator is used for any problem
         Model model = new Model("xRoundRobinGenerator");
-        IntVar[][][] matchdays = new IntVar[amountOfMatchDays][AMOUNT_OF_TEAMS][AMOUNT_OF_TEAMS];
+        IntVar[][][] matchdays = new IntVar[amountOfMatchDays][amountOfTeams][amountOfTeams];
         //seed the tensor with variables
-        seedRobinTensor(amountOfMatchDays, model, matchdays);
+        seedRobinTensor(amountOfMatchDays, model, matchdays, amountOfTeams);
         watch.split();
         System.out.println("seed after " + watch.getSplitTime());
         //constraint 1: A team never plays itself
-        addNeverPlayYourselfConst(amountOfMatchDays, model, matchdays);
+        addNeverPlayYourselfConst(amountOfMatchDays, model, matchdays, amountOfTeams);
         watch.split();
         System.out.println("never Play yourself after " + watch.getSplitTime());
         //constraint 2: A team can only play one game each matchday
-        playMaxOneGamePerMatchday(amountOfMatchDays, model, matchdays);
+        playMaxOneGamePerMatchday(amountOfMatchDays, model, matchdays, amountOfTeams);
         watch.split();
         System.out.println("play one each matchday " + watch.getSplitTime());
         //constraint 3: After the full tournament, every team has played every team twice
-        everyonePlaysEachoterTwice(amountOfMatchDays, model, matchdays);
+        everyonePlaysEachoterTwice(amountOfMatchDays, model, matchdays, amountOfTeams);
         //constraint 4: At the halfway point, each team should have played every team once (general rule, after amountOfMatchdays/2 for all n)
         watch.split();
         System.out.println("Play eachother twice " + watch.getSplitTime());
-        halfWayPointConstraint(amountOfMatchDays, matchdays, model);
+        halfWayPointConstraint(amountOfMatchDays, matchdays, model, amountOfTeams);
         watch.split();
         System.out.println("Amount of matchdays " + watch.getSplitTime());
         //constraint 5: Optimization of the home and away pattern
@@ -49,20 +50,21 @@ public class DoubleRoundRobinGenerator {
         //MiniZinc black magic
         int counter = 0;
         model.getSolver().limitSolution(10);
-        while (model.getSolver().solve()) {
-            prettyPrintSolution(matchdays, amountOfMatchDays);
+        while (model.getSolver().solve()&& counter<amountOfSamples) {
+            prettyPrintSolution(matchdays, amountOfMatchDays, amountOfTeams);
+            CsvWriter.createSample(matchdays,amountOfMatchDays,amountOfTeams,counter);
             counter++;
         }
-        System.out.println("Amount of valid solutions for " + AMOUNT_OF_TEAMS + " teams: " + counter);
+        System.out.println("Amount of valid solutions for " + amountOfTeams + " teams: " + counter);
 
     }
 
 
 
 
-    private static void everyonePlaysEachoterTwice(int amountOfMatchDays, Model model, IntVar[][][] matchdays) {
-        for (int i = 0; i < AMOUNT_OF_TEAMS; i++) {
-            for (int j = 0; j < AMOUNT_OF_TEAMS; j++) {
+    private static void everyonePlaysEachoterTwice(int amountOfMatchDays, Model model, IntVar[][][] matchdays, int amountOfTeams) {
+        for (int i = 0; i < amountOfTeams; i++) {
+            for (int j = 0; j < amountOfTeams; j++) {
                 if (i != j) {
 
                     //grab element (i,j) in each dimension of the tensor
@@ -86,15 +88,15 @@ public class DoubleRoundRobinGenerator {
 
     /**
      * Puts constraints into the model to make sure every team does not play itself
-     *
-     * @param amountOfMatchDays
+     *  @param amountOfMatchDays
      * @param model
      * @param matchdays
+     * @param amountOfTeams
      */
-    private static void addNeverPlayYourselfConst(int amountOfMatchDays, Model model, IntVar[][][] matchdays) {
+    private static void addNeverPlayYourselfConst(int amountOfMatchDays, Model model, IntVar[][][] matchdays, int amountOfTeams) {
         for (int h = 0; h < amountOfMatchDays; h++) {
-            for (int i = 0; i < AMOUNT_OF_TEAMS; i++) {
-                for (int j = 0; j < AMOUNT_OF_TEAMS; j++) {
+            for (int i = 0; i < amountOfTeams; i++) {
+                for (int j = 0; j < amountOfTeams; j++) {
                     //this wil make sure the trace of every matchday 2D tensor is 0
                     if (i == j) {
                         model.arithm(matchdays[h][i][j], "=", 0).post();
@@ -106,20 +108,20 @@ public class DoubleRoundRobinGenerator {
 
     /**
      * Puts the constraint that in each dimension of the tensor, the sum of row i + col i <= 1
-     *
-     * @param amountOfMatchDays
+     *  @param amountOfMatchDays
      * @param model
      * @param matchdays
+     * @param amountOfTeams
      */
-    private static void playMaxOneGamePerMatchday(int amountOfMatchDays, Model model, IntVar[][][] matchdays) {
+    private static void playMaxOneGamePerMatchday(int amountOfMatchDays, Model model, IntVar[][][] matchdays, int amountOfTeams) {
 
         for (int day = 0; day < amountOfMatchDays; day++) {
-            for (int teamIndex = 0; teamIndex < AMOUNT_OF_TEAMS; teamIndex++) {
+            for (int teamIndex = 0; teamIndex < amountOfTeams; teamIndex++) {
                 //grab the  row
                 List<IntVar> home = Arrays.asList(matchdays[day][teamIndex]);
                 //grab the corresponding column
                 List<IntVar> away = new ArrayList<>();
-                for (int i = 0; i < AMOUNT_OF_TEAMS; i++) {
+                for (int i = 0; i < amountOfTeams; i++) {
                     away.add(matchdays[day][i][teamIndex]);
                 }
                 //combine them
@@ -134,12 +136,12 @@ public class DoubleRoundRobinGenerator {
         }
     }
 
-    private static void halfWayPointConstraint(int amountOfMatchDays, IntVar[][][] matchdays, Model model) {
+    private static void halfWayPointConstraint(int amountOfMatchDays, IntVar[][][] matchdays, Model model, int amountOfTeams) {
 
 
         int amountOfMatchdaysInFirstCycle = amountOfMatchDays / 2;
-        for (int x = 0; x < AMOUNT_OF_TEAMS; x++) {
-            for (int y = 0; y < AMOUNT_OF_TEAMS; y++) {
+        for (int x = 0; x < amountOfTeams; x++) {
+            for (int y = 0; y < amountOfTeams; y++) {
                 if (x != y) {
                     ArrayList<IntVar> list = new ArrayList<IntVar>();
                     for (int i = 0; i < amountOfMatchdaysInFirstCycle; i++) {
@@ -155,24 +157,24 @@ public class DoubleRoundRobinGenerator {
         }
     }
 
-    private static void homeAndAwayPattern(int amountOfMatchDays,IntVar[][][] matchdays,Model model){
-        int halfwaypoint = (amountOfMatchDays) / 2;
-        for(int teamindex = 0; teamindex<AMOUNT_OF_TEAMS; teamindex++){
-            for(int day=1;day<=halfwaypoint ;day++){
-                List<IntVar> list = new ArrayList<>(Arrays.asList(matchdays[day][teamindex]));
-                list.addAll(Arrays.asList(matchdays[day-1][teamindex]));
-                model.sum(convertVarListToArray(list),"<=",1).post();
-            }
-        }
-
-
-    }
+//    private static void homeAndAwayPattern(int amountOfMatchDays,IntVar[][][] matchdays,Model model){
+//        int halfwaypoint = (amountOfMatchDays) / 2;
+//        for(int teamindex = 0; teamindex<amountOfTeams; teamindex++){
+//            for(int day=1;day<=halfwaypoint ;day++){
+//                List<IntVar> list = new ArrayList<>(Arrays.asList(matchdays[day][teamindex]));
+//                list.addAll(Arrays.asList(matchdays[day-1][teamindex]));
+//                model.sum(convertVarListToArray(list),"<=",1).post();
+//            }
+//        }
+//
+//
+//    }
 
 
 //    private static void homeAndAwayPattern(int amountOfMatchDays, IntVar[][][] matchdays, Model model) {
 //        //we split this in 2 different constraints who partially implicate eachother, first of all: you can't play 2 games in a row at home
 //
-//            for (int teamIndex = 0; teamIndex < AMOUNT_OF_TEAMS; teamIndex++) {
+//            for (int teamIndex = 0; teamIndex < amountOfTeams; teamIndex++) {
 //                for (int day = 1; day < amountOfMatchDays; day++) {
 //                    List<IntVar> intVarList = new ArrayList<IntVar>(Arrays.asList(matchdays[day][teamIndex]));
 //                    intVarList.addAll(Arrays.asList(matchdays[day - 1][teamIndex]));
@@ -183,10 +185,10 @@ public class DoubleRoundRobinGenerator {
 
         //now we do the same for columns, so we don't want 2 away games in a row
 
-//        for(int teamindex = 0; teamindex<AMOUNT_OF_TEAMS; teamindex++){
+//        for(int teamindex = 0; teamindex<amountOfTeams; teamindex++){
 //            for(int day = 1; day<amountOfMatchDays;day++){
 //                List<IntVar> intVarList = new ArrayList<IntVar>(Arrays.asList(matchdays[day][teamindex]));
-//                for(int j = 0; j<AMOUNT_OF_TEAMS;j++){
+//                for(int j = 0; j<amountOfTeams;j++){
 //                    intVarList.add(matchdays[day][j][teamindex]);
 //                    intVarList.add(matchdays[day-1][j][teamindex]);
 //                }
@@ -212,15 +214,15 @@ public class DoubleRoundRobinGenerator {
     /**
      * Seeds the roundRobin tensor with an upper and lowerbound (0-1) booleanwise
      * provide eacht IntVar with unique identifier: e.g the value of 0,0,0 means whether on matchday 0 team 0 played team 0
-     *
-     * @param amountOfMatchDays the depth dimension
+     *  @param amountOfMatchDays the depth dimension
      * @param model             the model that has the tensor injected
      * @param matchdays         the tensor to seed
+     * @param amountOfTeams
      */
-    private static void seedRobinTensor(int amountOfMatchDays, Model model, IntVar[][][] matchdays) {
+    private static void seedRobinTensor(int amountOfMatchDays, Model model, IntVar[][][] matchdays, int amountOfTeams) {
         for (int h = 0; h < amountOfMatchDays; h++) {
-            for (int i = 0; i < DoubleRoundRobinGenerator.AMOUNT_OF_TEAMS; i++) {
-                for (int j = 0; j < DoubleRoundRobinGenerator.AMOUNT_OF_TEAMS; j++) {
+            for (int i = 0; i < amountOfTeams; i++) {
+                for (int j = 0; j < amountOfTeams; j++) {
                     //every element in the  represents a boolean, 0 and 1, we declare every element in the matrix with a unique combination
                     matchdays[h][i][j] = model.intVar(h + "," + i + "," + j, 0, 1);
                 }
@@ -228,12 +230,12 @@ public class DoubleRoundRobinGenerator {
         }
     }
 
-    private static void prettyPrintSolution(IntVar[][][] matchdays, int amountOfMatchDays) {
+    private static void prettyPrintSolution(IntVar[][][] matchdays, int amountOfMatchDays, int amountOfTeams) {
         for (int h = 0; h < amountOfMatchDays; h++) {
             System.out.println("matchday " + h);
             IntVar[][] matchday = matchdays[h];
-            for (int i = 0; i < AMOUNT_OF_TEAMS; i++) {
-                for (int j = 0; j < AMOUNT_OF_TEAMS; j++) {
+            for (int i = 0; i < amountOfTeams; i++) {
+                for (int j = 0; j < amountOfTeams; j++) {
                     System.out.print(matchday[i][j].getValue() + " ");
                 }
                 System.out.println();
@@ -245,11 +247,12 @@ public class DoubleRoundRobinGenerator {
 
     /**
      * Calculates the amount of games in the round robin tournament
+     * @param amountOfTeams
      */
-    private static int calculateMatchDays() {
-        if (DoubleRoundRobinGenerator.AMOUNT_OF_TEAMS % 2 == 0)
-            return (DoubleRoundRobinGenerator.AMOUNT_OF_TEAMS - 1) * 2;
-        else return DoubleRoundRobinGenerator.AMOUNT_OF_TEAMS * 2;
+    private static int calculateMatchDays(int amountOfTeams) {
+        if (amountOfTeams % 2 == 0)
+            return (amountOfTeams - 1) * 2;
+        else return amountOfTeams * 2;
     }
 
     /**
